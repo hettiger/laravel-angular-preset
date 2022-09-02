38844e9ae4e5f7f3b1693693c39b75d72150b9f2
Martin Hettiger <martin@hettiger.com>
2022-09-02 16:28:59 +0200

    Install Angular

--- src/Commands/LaravelAngularPresetCommand.php
+++ src/Commands/LaravelAngularPresetCommand.php
@@ -6,14 +6,26 @@ use Illuminate\Console\Command;
 
 class LaravelAngularPresetCommand extends Command
 {
-    public $signature = 'laravel-angular-preset';
+    public $signature = 'laravel-angular-preset:install';
 
-    public $description = 'My command';
+    public $description = 'Installs angular frontend preset';
 
     public function handle(): int
     {
+        $this->installAngular();
+
         $this->comment('All done');
 
         return self::SUCCESS;
+    }
+
+    protected function installAngular()
+    {
+        $this->info('Installing Angular to `resources/angular`');
+
+        $this->withProgressBar([
+            fn() => exec('npm install -D @angular/cli'),
+            fn() => exec('cd resources && ng new angular'),
+        ], fn($action) => $action());
     }
 }
